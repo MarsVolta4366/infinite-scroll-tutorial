d017bb0ddd0e2e46307eae2088305136e67d2aa1
unknown <derek.slauson5@gmail.com>
2022-07-23 09:20:37 -0500

    Infinite scrolling is implemented

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import { useState } from "react"
+// https://www.youtube.com/watch?v=NZKUirTtxcg
+
+import { useState, useRef, useCallback } from "react"
 import useBookSearch from "./useBookSearch"
 
 function App() {
@@ -8,6 +10,24 @@ function App() {
 
   const { loading, error, books, hasMore } = useBookSearch(query, pageNumber)
 
+  const observer = useRef()
+  const lastBookElementRef = useCallback(node => {
+    if (loading) {
+      return
+    }
+    if (observer.current) {
+      observer.current.disconnect()
+    }
+    observer.current = new IntersectionObserver(entries => {
+      if (entries[0].isIntersecting && hasMore) {
+        setPageNumber(prevPageNumber => prevPageNumber + 1)
+      }
+    })
+    if (node) {
+      observer.current.observe(node)
+    }
+  }, [loading, hasMore])
+
   const handleSearch = (e) => {
     setQuery(e.target.value)
     setPageNumber(1)
@@ -15,9 +35,13 @@ function App() {
 
   return (
     <>
-      <input type="text" onChange={handleSearch}></input>
-      {books.map(book => {
-        return <div key={book}>{book}</div>
+      <input type="text" value={query} onChange={handleSearch}></input>
+      {books.map((book, index) => {
+        if (books.length === index + 1) {
+          return <div ref={lastBookElementRef} key={book}>{book}</div>
+        } else {
+          return <div key={book}>{book}</div>
+        }
       })}
       <div>{loading && "Loading..."}</div>
       <div>{error && "Error..."}</div>
